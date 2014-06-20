701b5e13957c42586cc2baf717dd43579007ca23
Miguel Angel Alarcos <miguel@localhost.localdomain>
2014-06-20 08:32:38 +0200

    added dictionary sets

--- main_twisted.py
+++ main_twisted.py
@@ -5,9 +5,11 @@ from twisted.internet import reactor, task
 from twisted.application import service
 from twisted.web import server
 
-from temperature import read_temp
+#from temperature import read_temp
+from time import time
 
-balbula_1 = True
+sets = {}
+sets['balbula_1'] = True
 input1 = 0.0
 
 
@@ -16,34 +18,30 @@ class DataResource(resource.Resource):
 
     def render_GET(self, request):
         request.setHeader("content-type", "application/json")
-        return dumps({'balbula_1': balbula_1, 'input1': input1})
+        return dumps({'time': time(), 'balbula_1': sets['balbula_1'], 'input1': input1})
 
 
-class SetBalbula_1(resource.Resource):
+class Set(resource.Resource):
     isLeaf = True
 
-    def render_GET(self, request):
-        global balbula_1
-        value = request.args['value'][0]
-        if value == '0':
-            balbula_1 = False
-        else:
-            balbula_1 = True
+    def render_POST(self, request):
+        variable = request.args['var'][0]
+        sets[variable] = not sets[variable]
         return ''
 
 
 def f():
     global input1
-    #input1 -= 1
-    #input1 += balbula_1*2
-    input1 = read_temp() or input1
+    input1 -= 1
+    input1 += sets['balbula_1']*2
+    #input1 = read_temp() or input1
 
 t = task.LoopingCall(f)
 t.start(1.0)
 
 root = resource.Resource()
 root.putChild('data', DataResource())
-root.putChild('set_balbula_1', SetBalbula_1())
+root.putChild('set', Set())
 
 application = service.Application('SCADA')
 service = reactor.listenTCP(8080, server.Site(root))
