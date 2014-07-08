ab40d7fbe93677e1c1c829fd31ab9d8d17d76f10
Miguel Ángel <miguel@debianHome.miguel.hom>
2014-07-08 18:32:19 +0200

    readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 [ ![Codeship Status for miguelalarcos/pySimulatorSCADA](https://www.codeship.io/projects/851267f0-e8ea-0131-9a19-6e35e4f3ef47/status)](https://www.codeship.io/projects/26118)
+
+readme
--- test_1.py
+++ test_1.py
@@ -1,6 +1,17 @@
+import main_twisted
 from main_twisted import *
+from mock import Mock
 
 def test_setUnset_Control():
     sets['control'] = False
     setUnset('control')
     assert sets['control'] == True
+
+def test_class_Set():
+    sets['control'] = False
+    c = Set()
+    request = Mock()
+    request.args = {'var': ['control']}
+    main_twisted.setUnset = Mock()
+    c.render_POST(request)
+    assert main_twisted.setUnset.called
